3ccb58c504e77554471c123cc5f74ade917dc78d
logbybulb <170230330+logbybulb@users.noreply.github.com>
2025-04-25 10:50:32 -0700

    pr

--- src/tree/TreeException.java
+++ src/tree/TreeException.java
@@ -1,6 +1,6 @@
-package trees;
+//package trees;
 
-public class TreeException extends Tree {
+public class TreeException extends Exception {
     public TreeException(String s) {
         // Pass s into a superclass ctor.
         super(s);
