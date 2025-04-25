7390c07832dcffc0b2e4d62da28920f51c8447d6
marljonson <marl.jonson@comcast.net>
2025-04-25 08:39:50 -0700

    git add all files

--- src/tree/TreeException.java
+++ src/tree/TreeException.java
@@ -1,7 +1,8 @@
 package trees;
 
-public class TreeException extends ??? {
+public class TreeException extends Tree {
     public TreeException(String s) {
         // Pass s into a superclass ctor.
+        super(s);
     }
 }
